963a4c123b80d3d355ba2793a26c567b0d760229
peterrhysthomas@yahoo.co.uk <peterrhysthomas@yahoo.co.uk>
2021-12-23 14:31:25 +0000

    Added logging

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,16 +1,24 @@
 package com.develogical;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class QueryProcessor {
 
+    private static Logger logger = LoggerFactory.getLogger(QueryProcessor.class);
+
     public String process(String query) {
+        logger.debug("Received query: " + query);
+        String result = "";
         if (query.toLowerCase().contains("shakespeare")) {
-            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+            result =  "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
         if (query.toLowerCase().contains("your name")){
-            return "pete";
+            result = "pete";
         }
-        return "";
+        logger.debug("Result is: " + result);
+        return result;
     }
 }
